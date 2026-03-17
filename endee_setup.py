f991f45f1a4c3354ada724f04fc9f239c08ae9ca
KARTHIK M <110376979+Karthikkarthik567@users.noreply.github.com>
2026-03-17 23:21:04 +0530

    Update endee_setup.py

--- endee_setup.py
+++ endee_setup.py
@@ -1,11 +1,11 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
 
+# Load model
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def load_data():
-   with open("medical_data.txt","r") as f:
+    with open("medical_data.txt", "r") as f:
         texts = f.readlines()
 
     embeddings = model.encode(texts)
@@ -15,6 +15,6 @@ texts, embeddings = load_data()
 
 def search(query):
     query_embedding = model.encode([query])
-    scores = cosine_similarity(query_embedding, embeddings)[0]
-    best_index = np.argmax(scores)
-    return texts[best_index]
+    similarities = cosine_similarity(query_embedding, embeddings)
+    index = similarities.argmax()
+    return texts[index]
